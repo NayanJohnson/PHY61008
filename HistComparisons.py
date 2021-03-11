be968c242ee614cf71aef3cd2c38ea3a96876358
Nayan Mark Johnson <nayan.mark.johnson@cern.ch>
2021-03-11 00:23:46 +0000

    Added comments to HistComparisons.py.

--- HistComparisons.py
+++ HistComparisons.py
@@ -67,38 +67,57 @@ for key in HistFile1.GetListOfKeys():
     Hist1.Draw("hist same")
     Hist2.Draw("hist same")
 
-    # Legend 
+    # Legend properties
     LegendX1 = .8
     LegendX_interval = 0.15
     LegendY1 = .95
     LegendY_interval = 0.1
 
     Legend1 = TLegend(LegendX1, LegendY1 , LegendX1+LegendX_interval, LegendY1-LegendY_interval)
+    
+    # Stops legend overwriting canvas
     SetOwnership(Legend1,False)
     Legend1.SetBorderSize(1)
     Legend1.SetShadowColor(2)
     Legend1.SetHeader("Cuts")
+    
+    # Entries
     Legend1.AddEntry("entries","Entries: "+str(int(Hist1.GetEntries())))
     Legend1.AddEntry(Hist1, "Line Color", "l")
+
     Legend1.SetTextSize(0.025)
     Legend1.SetTextColor(1)
+
+    # Seperation is small, but will be maximised to the bounds of the TLegend
+    # box
     Legend1.SetEntrySeparation(.1)
     Legend1.Draw("same")
 
     Legend2 = TLegend(LegendX1, LegendY1-LegendY_interval , LegendX1+LegendX_interval, LegendY1-2*LegendY_interval)
+    
+    # Stops legend overwriting canvas    
     SetOwnership(Legend2,False)
     Legend2.SetBorderSize(1)
     Legend2.SetShadowColor(2)
     Legend2.SetHeader("No Cuts")
+    
+    # Entries
     Legend2.AddEntry("entries","Entries: "+str(int(Hist2.GetEntries())))
     Legend2.AddEntry(Hist2, "Line Color", "l")
+
     Legend2.SetTextSize(0.025)
     Legend2.SetTextColor(1)
+    
+    # Seperation is small, but will be maximised to the bounds of the TLegend
+    # box    
     Legend2.SetEntrySeparation(.1)
     Legend2.Draw("same")
+
+    # Update canvas
     HistCan.Update()
 
+    # Write canvas to outfile, needs the name for some reason.
     outfile.WriteObject(HistCan, histname)
 
-# Writing and closing file
+# Closing file
 outfile.Close()
