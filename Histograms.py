3e07e8d7b2945ba961f2cba68861ac5e19df651c
Nayan Mark Johnson <nayan.mark.johnson@cern.ch>
2021-03-15 14:03:41 +0000

    Making patch for WZEmJ_WZ_MuVMuMu_Cuts.

--- Histograms.py
+++ Histograms.py
@@ -24,35 +24,46 @@ HistDict = funcs.MakeHists(HistDict)
 # Looping through events
 for EventNum in range(myTree['NEvents']):
 
-    HistDict, ParticleDict, EventDict = funcs.GetParticles(myTree, HistDict, EventNum)
+    HistDict, ParticleDict = funcs.GetParticles(myTree, HistDict, EventNum)
 
-    # Event level selection for WWEmJ_WW_Muons
-    if EventDict['Count']['Electrons'] > 0 and EventDict['Count']['Muons'] >= 2:
+    # Setting FinalBeamElectron
+    ParticleDict['FinalBeamElectron'] = ParticleDict['LeadingElectron']
+
+    # Event level selection for WWEmJ_WZ_MuVMuMu
+    if EventDict['Count']['Electrons'] > 0 and EventDict['Count']['Muons'] >= 3:
         ParticleDict['FinalBeamElectron'] = ParticleDict['LeadingElectron']
         
-        # WPlus and WMinus hists
-        # Leading and SubLeading muons will always be from the W bosons 
-        # (in this process) so I seperate them by charge to determine which
-        # boson they came from
+        # Seperating out boson muons 
+        # Only attempt this if all muons are present
+        ZMuonInvMassList = []
+        if ParticleDict['LeadingMuon']['Check'] and ParticleDict['SubLeadingMuon']['Check'] and ParticleDict['ThirdMuon']['Check']:  
+            MuonPermutations = [
+                (ParticleDict['LeadingMuon'], ParticleDict['SubLeadingMuon'], ParticleDict['ThirdMuon']),
+                (ParticleDict['LeadingMuon'], ParticleDict['ThirdMuon'], ParticleDict['SubLeadingMuon']),
+                (ParticleDict['SubLeadingMuon'], ParticleDict['ThirdMuon'], ParticleDict['LeadingMuon'])
+            ]
 
-        # Will only run if that muon is present
-        for Muon in [ParticleDict['LeadingMuon'], ParticleDict['SubLeadingMuon']]:
-            if Muon['Check']:    
-                if Muon['PID'] == 13:
-                    ParticleDict = funcs.AddParticle('WMinusMuon', ParticleDict, Muon['P4'])
-                elif Muon['PID'] == -13:
-                    ParticleDict = funcs.AddParticle('WPlusMuon', ParticleDict, Muon['P4'])
+            # Calculating InvMass of the three different muon pairs
+            for MuonPair in MuonPermutations:
+                ZMuonInvMassList.append(funcs.GetParticleVariable('InvMass', MuonPair[0:2]))
 
+            # Find the closest InvMass to the Z mass
+            ZMuonPairInvMass = min(ZMuonInvMassList, key=lambda x:abs(x-91.1876))
+            ZMuonPairIndex = ZMuonInvMassList.index(ZMuonPairInvMass)
+            if MuonPermutations[ZMuonPairIndex][0]['Pt'] < MuonPermutations[ZMuonPairIndex][1]['Pt']:
+                ParticleDict['ZLeadingMuon'] = MuonPermutations[ZMuonPairIndex][1]
+                ParticleDict['ZSubLeadingMuon'] = MuonPermutations[ZMuonPairIndex][0]
+            else:        
+                ParticleDict['ZSubLeadingMuon'] = MuonPermutations[ZMuonPairIndex][1]
+                ParticleDict['ZLeadingMuon'] = MuonPermutations[ZMuonPairIndex][0]
+            ParticleDict['WMuon'] = MuonPermutations[ZMuonPairIndex][2]
 
-        # Filling HistDict with particles then filling the hists
-        HistDict = funcs.RequestParticles(HistDict, ParticleDict)
-        funcs.FillHists(HistDict)
+    # Filling HistDict with particles then filling the hists
+    HistDict = funcs.RequestParticles(HistDict, ParticleDict)
+    funcs.FillHists(HistDict)
 
-# Get scaling factor for histograms
-Scale = funcs.GetScale('tag_1_pythia.log', myTree['NEvents'])
-
-# Scaling and altering hist lims
-funcs.HistLims(HistDict, Scale)
+# Rescaling hist lims
+funcs.HistLims(HistDict)
 
 # Writing and closing file
 outfile.Write()
