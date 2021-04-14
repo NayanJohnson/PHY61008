36f650454f5d1c4b2c67f09931aba76757b0cbc8
Nayan Mark Johnson <nayan.mark.johnson@cern.ch>
2021-04-14 01:34:56 +0100

    Fix to HistComparison file names.

--- HistComparisons.py
+++ HistComparisons.py
@@ -96,14 +96,14 @@ for FilePair in FileComparisons:
                         HistFile1_LoopRun = LoopRunPair[0]
                         HistFile1_EventRun = EventRunPair[0]
                         HistFile1_AnalysisRun = AnalysisRunPair[0]
-                        HistFile1_Name = HistFile1_Prefix+'_'+HistFile1_LevelRun+'Level_Loop'+HistFile1_LoopRun+'Event'+HistFile1_EventRun+'Analysis'+HistFile1_AnalysisRun
+                        HistFile1_Name = RootDir+HistFile1_Prefix+'_'+HistFile1_LevelRun+'Level_Loop'+HistFile1_LoopRun+'Event'+HistFile1_EventRun+'Analysis'+HistFile1_AnalysisRun
 
                         HistFile2_Prefix = FilePair[1]
                         HistFile2_LevelRun = LevelRunPair[1]
                         HistFile2_LoopRun = LoopRunPair[1]
                         HistFile2_EventRun = EventRunPair[1]
                         HistFile2_AnalysisRun = AnalysisRunPair[1]
-                        HistFile2_Name = HistFile2_Prefix+'_'+HistFile2_LevelRun+'Level_Loop'+HistFile2_LoopRun+'Event'+HistFile2_EventRun+'Analysis'+HistFile2_AnalysisRun
+                        HistFile2_Name = RootDir+HistFile2_Prefix+'_'+HistFile2_LevelRun+'Level_Loop'+HistFile2_LoopRun+'Event'+HistFile2_EventRun+'Analysis'+HistFile2_AnalysisRun
                         
                         if Norm:
                             Comparison = 'Norm'
