3580ef568f5735fd781be4a87677763eebdb1573
Nayan Mark Johnson <nayan.mark.johnson@cern.ch>
2021-04-27 17:51:15 +0100

    Adding ZLeading-FinalBeamElectron dR_Eta cut to config.

--- config.py
+++ config.py
@@ -59,6 +59,11 @@ EventLoopParams = {
                     'M'                 :   (0, float('inf')),
                 },
 
+                'ZLeading_FinalBeam_Jets' 
+                                    :   {
+                    'dR_Eta'            :   (0, float('inf')),
+                },       
+
                 'MissingET'         :   {
                     'Et'                :   (0, float('inf')),
                 },
@@ -67,8 +72,8 @@ EventLoopParams = {
                     'Eta'               :   (float('-inf'), float('inf')),
                 },
 
-                'FinalBeamJet' :   {
-                    'Pt'               :   (0, float('inf')),
+                'FinalBeamJet'      :   {
+                    'Pt'                :   (0, float('inf')),
                 },                                
             },
 
@@ -86,6 +91,11 @@ EventLoopParams = {
                     'M'                 :   (0, float('inf')),
                 },
 
+                'ZLeading_FinalBeam_Jets' 
+                                    :   {
+                    'dR_Eta'            :   (0, float('inf')),
+                },       
+
                 'MissingET'         :   {
                     'Et'                :   (0, float('inf')),
                 },
@@ -94,8 +104,8 @@ EventLoopParams = {
                     'Eta'               :   (float('-inf'), float('inf')),
                 },
 
-                'FinalBeamJet' :   {
-                    'Pt'               :   (0, float('inf')),
+                'FinalBeamJet'      :   {
+                    'Pt'                :   (0, float('inf')),
                 },      
             },
         },
