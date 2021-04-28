01d37c9d3a657fdc73806ca1e1e6660c599af782
Nayan Mark Johnson <nayan.mark.johnson@cern.ch>
2021-04-28 14:38:38 +0100

    Adding ZSubLeading-FinalBeamElectron dR_Eta, M cuts to config.

--- config.py
+++ config.py
@@ -64,7 +64,14 @@ EventLoopParams = {
                                     :   {
                     'dR_Eta'            :   (0, float('inf')),
                     'M'                 :   (0, float('inf')),
-                },                       
+                },             
+
+                'ZSubLeading_FinalBeam_Jets' 
+                                    :   {
+                    'dR_Eta'            :   (0, float('inf')),
+                    'M'                 :   (0, float('inf')),
+                },       
+
 
                 'MissingET'         :   {
                     'Et'                :   (0, float('inf')),
@@ -94,6 +101,12 @@ EventLoopParams = {
                 },
 
                 'ZLeading_FinalBeam_Jets' 
+                                    :   {
+                    'dR_Eta'            :   (0, float('inf')),
+                    'M'                 :   (0, float('inf')),
+                },     
+
+                'ZSubLeading_FinalBeam_Jets' 
                                     :   {
                     'dR_Eta'            :   (0, float('inf')),
                     'M'                 :   (0, float('inf')),
